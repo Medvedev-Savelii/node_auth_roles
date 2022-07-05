2d5e46e5e147b5d6da9301448dfce85d04cb2778
sava <savamedvedev@gmail.com>
2022-07-05 23:56:43 +0800

    THIRD-commit

--- authController.js
+++ authController.js
@@ -3,10 +3,25 @@ const Role = require("./models/Role");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { validationResult } = require("express-validator");
+const { secret } = require("./config");
+
+const generateAccessToken = (id, roles) => {
+  const payload = {
+    id,
+    roles,
+  };
+  return jwt.sign(payload, secret, { expiresIn: "24h" });
+};
 
 class authController {
   async registration(req, res) {
     try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res
+          .status(400)
+          .json({ message: "Ошибка при регистрации", errors });
+      }
       const { username, password } = req.body;
       const candidate = await User.findOne({ username });
       if (candidate) {
@@ -31,6 +46,18 @@ class authController {
   async login(req, res) {
     try {
       const { username, password } = req.body;
+      const user = await User.findOne({ username });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ message: `Пользователь ${username} не найден` });
+      }
+      const validPassword = bcrypt.compareSync(password, user.password);
+      if (!validPassword) {
+        return res.status(400).json({ message: `Введен неверный пароль` });
+      }
+      const token = generateAccessToken(user._id, user.roles);
+      return res.json({ token });
     } catch (error) {
       console.log(error);
       res.status(400).json({ message: "Login error!!!" });
@@ -38,7 +65,8 @@ class authController {
   }
   async getUsers(req, res) {
     try {
-      res.json("Server worker");
+      const users = await User.find();
+      res.json(users);
     } catch (error) {
       console.log(error);
     }
--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require("express");
 const mongoose = require("mongoose");
 const PORT = 5000;
 const authRouter = require("./authRouter");
+
 const URL_DB =
   "mongodb+srv://admin:123@cluster0.ahxvvop.mongodb.net/auth_role?retryWrites=true&w=majority";
+
 const app = express();
+
 app.use(express.json());
 app.use("/auth", authRouter);
+
 const start = async () => {
   try {
     await mongoose.connect(URL_DB);
