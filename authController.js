7846c9835d9c77141a4fe8f2d72cd842d366f2d0
sava <savamedvedev@gmail.com>
2022-07-05 21:56:27 +0800

    first-commit

---
+++ authController.js
@@ -0,0 +1,23 @@
+class authController {
+  async registration(req, res) {
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async login(req, res) {
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async getUsers(req, res) {
+    try {
+      res.json("Server worker");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+module.exports = new authController();
---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const PORT = 5000;
+const authRouter = require("./authRouter");
+const URL_DB =
+  "mongodb+srv://admin:123@cluster0.ahxvvop.mongodb.net/auth_role?retryWrites=true&w=majority";
+const app = express();
+app.use(express.json());
+app.use("/auth", authRouter);
+const start = async () => {
+  try {
+    await mongoose.connect(URL_DB);
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
